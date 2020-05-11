070120a0b6b587bc1613aebb3275359cc63af1eb
kevin <caoyong@bobandata.com>
2020-05-11 09:59:02 +0800

    cep test

--- src/main/java/com/kevin/study/flink/cep/SimpleConditionsTest.java
+++ src/main/java/com/kevin/study/flink/cep/SimpleConditionsTest.java
@@ -5,15 +5,18 @@ import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.pattern.Pattern;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
 
 
 import com.kevin.study.flink.Event;
 
-import java.util.ArrayList;
+import javax.annotation.Nullable;
+
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -35,17 +38,16 @@ public class SimpleConditionsTest {
         //输入数据源
         DataStream<Event> input = streamEnv.fromCollection(Arrays.asList(
                 new Event(1L, "a1", "add", 1588298400L),
-                new Event(2L, "c1", "add", 1588298400L),
-                new Event(3L, "a2", "add", 1588298400L),
-                new Event(4L, "b1", "add", 1588298400L),
-                new Event(5L, "b1", "add", 1588298400L),
-                new Event(6L, "a3", "add", 1588298400L)
+                new Event(2L, "a2", "add", 1588298402L),
+                new Event(3L, "a12", "add", 1588298400L),
+                new Event(4L, "b11", "add", 1588298412L),
+                new Event(5L, "b12", "add", 1588298400L),
+                new Event(6L, "c3", "add", 1588298400L),
+                new Event(7L, "b13", "add", 1588298400L)
         ));
 
         //1、定义规则  匹配以a和c开头的用户
-//        Pattern<Event, Event> pattern = getWhereOrPattern();
-
-        Pattern<Event, Event> pattern = getTimesPattern();
+        Pattern<Event, Event> pattern = getGreedyPattern();
 
         //2、模式检测
         PatternStream<Event> patternStream = CEP.pattern(input, pattern);
@@ -58,9 +60,11 @@ public class SimpleConditionsTest {
                         StringBuffer sb = new StringBuffer();
                         for (Map.Entry<String, List<Event>> entry : map.entrySet()) {
                             Iterator<Event> iterator = entry.getValue().iterator();
-                            iterator.forEachRemaining(i -> sb.append(i.getId()).append(","));
+                            iterator.forEachRemaining(i -> sb.append(i.getName()).append(","));
+                            sb.append("|").append(",");
                         }
-                        sb.deleteCharAt(sb.length() - 1);
+                        sb.delete(sb.length() - 3 , sb.length());
+//                        sb.deleteCharAt(sb.length() - 1);
                         return sb.toString();
                     }
                 }
@@ -70,33 +74,152 @@ public class SimpleConditionsTest {
 
     }
 
-    private static Pattern<Event, Event> getTimesPattern() {
+    private static Pattern<Event, Event> getNotFollowedBy() {
         return Pattern.<Event>begin("start").where(
-                    new SimpleCondition<Event>() {
-                        @Override
-                        public boolean filter(Event event) throws Exception {
-                            return event.getName().startsWith("a");
-                        }
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
                     }
-            ).times(2);
+                }
+        ).times(1, 2).notFollowedBy("notFollowed").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("b");
+                    }
+                }
+        ).next("next").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        );
+    }
+
+    private static Pattern<Event, Event> getNotNextPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }
+        ).times(1, 2).notNext("notNext").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        );
+    }
+
+    private static Pattern<Event, Event> getUntilPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }
+        ).oneOrMore().until(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("b");
+                    }
+                }
+        );
+    }
+
+    private static Pattern<Event, Event> getFollowedByAnyPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }
+        ).times(1, 2).followedByAny("followedByAny").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        ).times(1, 2);
+    }
+
+    private static Pattern<Event, Event> getFollowedByPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }).times(1, 2).followedBy("followedBy").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        ).times(1, 2);
+    }
+
+    private static Pattern<Event, Event> getNextPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }).times(1, 2).next("middle").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        ).times(1, 2);
+    }
+
+    private static Pattern<Event, Event> getGreedyPattern() {
+        return Pattern.<Event>begin("start").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
+                    }
+                }
+        ).times(2, 3).greedy().next("middle").where(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().length() == 3;
+                    }
+                }).times(1, 2);
     }
 
     private static Pattern<Event, Event> getWhereOrPattern() {
         return Pattern.<Event>begin("start").where(
-                    new SimpleCondition<Event>() {
-                        @Override
-                        public boolean filter(Event event) throws Exception {
-                            return event.getName().startsWith("a");
-                        }
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("a");
                     }
-            ).or(
-                            new SimpleCondition<Event>() {
-                                @Override
-                                public boolean filter(Event event) throws Exception {
-                                    return event.getName().startsWith("c");
-                                }
-                            }
-                    );
+                }
+        ).or(
+                new SimpleCondition<Event>() {
+                    @Override
+                    public boolean filter(Event event) throws Exception {
+                        return event.getName().startsWith("c");
+                    }
+                }
+        );
     }
 
 }
